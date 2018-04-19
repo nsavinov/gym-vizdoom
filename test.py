8ac84deb48150302b6543e6f59369eeaf5eab214
Nikolay Savinov <nsavinov@cvg-desktop-15.inf.ethz.ch>
2018-04-19 18:24:50 +0200

    .

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ for i_episode in range(2):
   for t in range(17000):
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
-    env.render(mode='human')
+    # env.render(mode='human')
     print(reward)
     if done:
       print("Episode finished after {} timesteps".format(t+1))
