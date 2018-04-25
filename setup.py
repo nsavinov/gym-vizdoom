eb61b13cdeaf8c13fcac763c77bd18f22eec6f0a
Nikolay Savinov <nsavinov@cvg-desktop-15.inf.ethz.ch>
2018-04-25 12:04:56 +0200

    added dependency

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_vizdoom',
       version='0.0.1',
-      install_requires=['gym'] #TODO: add vizdoom as well
+      install_requires=['gym', 'vizdoom']
 )
